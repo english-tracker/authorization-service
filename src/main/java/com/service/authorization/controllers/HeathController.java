4d633fd3abfc3815df64c85929c3a02038b799b0
Truong Nguyen Huy <truongnh@192.168.1.2.non-exists.ptr.local>
2025-02-20 22:58:59 +0700

    complete

---
+++ src/main/java/com/service/authorization/controllers/HeathController.java
@@ -0,0 +1,16 @@
+package com.service.authorization.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/authorization")
+public class HeathController {
+
+    @GetMapping("/heath")
+    public ResponseEntity<String> heath() {
+        return ResponseEntity.ok("OK!");
+    }
+}
